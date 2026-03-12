134ab16c5e88eb494bd6c5bac860bba6a088a96d
AL2002MI08 <alexandramizero@gmail.com>
2026-03-12 20:09:10 +0200

    refactor: handle case when the function is not an integer

--- exercise-6.py
+++ exercise-6.py
@@ -20,6 +20,8 @@
 # >>>>>>>>>>>>>
 # ============================================================================
 def number_pattern(n):
+    if type(n) == int:
+        return "Argument must be an integer value"
     if n < 1:
         return "Argument must be an integer greater than 0"
     output = ''
