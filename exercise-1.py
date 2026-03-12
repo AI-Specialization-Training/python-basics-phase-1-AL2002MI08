2b641f384b73576d25adbec4d449605df5de5d34
AL2002MI08 <alexandramizero@gmail.com>
2026-03-12 19:42:41 +0200

    feat: create function that returns hello world

--- exercise-1.py
+++ exercise-1.py
@@ -1,8 +1,6 @@
 # ============================================================================
 # TODO: Write a hello function that takes no arguments and returns "Hello!"
 # ============================================================================
-
-# Note: You can use print() for debugging purposes, but what matters is
-#       what the function returns, not what it prints.
-# ============================================================================
+def greet():
+    return "Hello World!"
 
