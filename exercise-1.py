0e0420ab9b818937a15bbd4f11a443b722e295a4
AL2002MI08 <alexandramizero@gmail.com>
2026-03-12 19:44:45 +0200

    refactor: rename function to hello

--- exercise-1.py
+++ exercise-1.py
@@ -2,5 +2,5 @@
 # TODO: Write a hello function that takes no arguments and returns "Hello!"
 # ============================================================================
 def hello():
-    return "Hello World!"
+    return "Hello!"
 
